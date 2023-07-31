23e9eb5ff2b7ce7aba6f4fbd2420412bf1f55c60
Tuannguyen1510 <87536692+Tuannguyen1510@users.noreply.github.com>
2023-07-31 21:08:24 +0700

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03._1_P1
+{
+    internal class Car
+    {
+        public string make;
+        public string model;
+        public string color;
+        public int year;
+
+
+
+        public void Start()
+        {
+            Console.WriteLine("Khởi động");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Dừng");
+        }
+        public void Input()
+        {
+            Console.WriteLine("Information car: make-{0}, model-{1}, color-{2}, year-{3}", make, model, color, year);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Lab03._1_P1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Car car = new Car();
+        car.make = "Hà Nội";
+        car.year = 2023;
+        car.model = "MR2";
+        car.color = "Black";
+
+        car.Input();
+        car.Start();
+        car.Stop();
+    }
+}
